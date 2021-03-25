bc2a6732d7be3a7adcfdc7002fdb23b2a78b8e47
sn0am <81282692+sn0am@users.noreply.github.com>
2021-03-25 02:37:14 -0700

    Update businessattributes.php

--- part2/businessattributes.php
+++ part2/businessattributes.php
@@ -1,10 +1,9 @@
-<?php
-?><head>
+<head>
 	<title>Completed Registration Form</title>
 	<style type="text/css">
 		h1 { color:  purple ; }
 		h2 { font-family: Times New Roman; color: blue ; }
-		p { font-family: Ariel; color: green}
+		p { font-family: Ariel; color: black}
 		input[type=submit] {
   			background-color: #FF0000; /* Red */
   			border: none;
@@ -21,91 +20,92 @@
 </head>
 <body>
 
-	<h1 style="text-align:center">Completed Form</h1>
+	<h1 style="text-align:center">REGISTRATION INFORMATION</h1>
 
 <?php
 		
-	if ((isset($_POST['T'])) && (isset($_POST['age'])))
-        {
-		$T = filter_var( $_POST['T'], FILTER_SANITIZE_STRING);
-		$age = filter_var( $_POST['age'], FILTER_SANITIZE_NUMBER_INT);
-		$fn = filter_var( $_POST['fn'], FILTER_SANITIZE_STRING);
-		$G = filter_var( $_POST['G'], FILTER_SANITIZE_STRING);
-		$ad = filter_var( $_POST['ad'], FILTER_SANITIZE_STRING);
-		$zip = filter_var( $_POST['zip'], FILTER_SANITIZE_NUMBER_INT);
-		$S = filter_var( $_POST['S'], FILTER_SANITIZE_STRING);
-		$city = filter_var( $_POST['city'], FILTER_SANITIZE_STRING);
+	if ((!empty($_POST['shirt'])) && !empty($_POST['age']) && !empty($_POST['fname']) && !empty($_POST['address']) && !empty($_POST['state']) && !empty($_POST['city']) && !empty($_POST['zip']) && !empty($_POST['gender']))
 		
-		if ($T == "SM") {
-			if ($age < 65) {
-				$price = 25 ;
-			} else if ($age > 65 ) {
-				$price = 20 ;
-		}		
-	}
-		if ($T == "MD") {
-			if ($age < 65) {
-				$price = 25 ;
-			} else if ($age >= 65 ) {
-				$price = 20 ;
-		}			
-	}
-		if ($T == "LG") {
-			if ($age < 65) {
-				$price = 25 ;
-			} else if ($age >= 65 ) {
-				$price = 20 ;
-		}	
-	}	
-		if ($T == "XL") {
-			if ($age < 65) {
-				$price = 25 ;
-			} else if ($age >= 65 ) {
-				$price = 20 ;
-		}	
-	}	
-		if ($T == "2XL") {
-			if ($age < 65) {
-				$price = 27 ;
-			} else if ($age >= 65 ) {
-				$price = 22 ;
-		}			
-	}
-		if ($T == "3XL") {
-			if ($age < 65) {
-				$price = 29 ;
-			} else if ($age >= 65 ) {
-				$price = 24 ;
-		}	
+		{
 		
-	}
-		echo "<div style=\"text-align:center\">";
-		echo "<p style='color: black ;'>Hey " . $fn . "!" . "</p>" ;
-		echo "<p style='color: black ;'>Based on the information you have provided, Your ticket price should be " . $price . "$" . "</p>" ;
-		echo "</br>";
-		echo "</br>";
-		echo "<p style='color: black ;'>Please verify that the information below is correct. If it is not, please go back and fill out the form properly to get accurate pricing information." . "</p>" ;
-		echo "<p style='color: black ;'>Address: " . $ad . ", " . $city  . ", " . $S . " " . $zip . "</p>";
-		echo "<p style='color: black ;'>Age: " . $age . "</p>";
-		echo "<p style='color: black ;'>Gender: " . $G . "</p>";
-		echo "<p style='color: black ;'>Shirt Size: " . $T . "</p>";
-		echo "</div>";
+			$shirt = filter_var( $_POST['shirt'], FILTER_SANITIZE_STRING);
+			$age = filter_var( $_POST['age'], FILTER_SANITIZE_NUMBER_INT);
+			$fname = filter_var( $_POST['fname'], FILTER_SANITIZE_STRING);
+			$gender = filter_var( $_POST['gender'], FILTER_SANITIZE_STRING);
+			$address = filter_var( $_POST['address'], FILTER_SANITIZE_STRING);
+			$zip = filter_var( $_POST['zip'], FILTER_SANITIZE_NUMBER_INT);
+			$state = filter_var( $_POST['state'], FILTER_SANITIZE_STRING);
+			$city = filter_var( $_POST['city'], FILTER_SANITIZE_STRING);
 		
+			if ($shirt == "SM") {
+				if ($age < 65) {
+					$price = 25 ;
+				} else if ($age > 65 ) {
+					$price = 20 ;
+				}		
+			}
+			if ($shirt == "MD") {
+				if ($age < 65) {
+					$price = 25 ;
+				} else if ($age >= 65 ) {
+					$price = 20 ;
+				}			
+			}
+			if ($shirt == "LG") {
+				if ($age < 65) {
+					$price = 25 ;
+				} else if ($age >= 65 ) {
+					$price = 20 ;
+				}	
+			}	
+			if ($shirt == "XL") {
+				if ($age < 65) {
+					$price = 25 ;
+				} else if ($age >= 65 ) {
+					$price = 20 ;
+				}	
+			}	
+			if ($shirt == "2XL") {
+				if ($age < 65) {
+					$price = 27 ;
+				} else if ($age >= 65 ) {
+					$price = 22 ;
+				}			
+
+			}
+			
+			echo "<div style=\"text-align:center\">";
+			echo "<p style='color: black ;'>Hey " . $fname . "!" . "</p>" ;
+			echo "<p style='color: black ;'>Based on the information you have provided, Your ticket price should be " . $price . "$" . "</p>" ;
+			echo "</br>";
+			echo "</br>";
+			echo "<p style='color: black ;'>Please verify that the information below is correct. If it is not, please go back and fill out the form properly to get accurate pricing information." . "</p>" ;
+			echo "<p style='color: black ;'>Address: " . $address . ", " . $city  . ", " . $state . " " . $zip . "</p>";
+			echo "<p style='color: black ;'>Age: " . $age . "</p>";
+			echo "<p style='color: black ;'>Gender: " . $gender . "</p>";
+			echo "<p style='color: black ;'>Shirt Size: " . $shirt . "</p>";
+			echo "</div>";
 		
 		
 		}
+		
 	else
-	{
-      		print "<p>Missing or invalid parameters. Please go back to the lab.html page to
-      		enter valid information.<br />";
-      		print "<a href='UnitConvertion.html'>Unit Converter Page</a>";
-	}
+		
+		{
+			echo "<div style=\"text-align:center\">";
+			echo "You have not properly completed some or all of the required information. Please go back to the registration page and try again!";
+			echo "</div>";
+		}
 ?>
-	<div style="text-align:center">
+		<div style="text-align:center">
+		
 			<form method="post" action="marathonreg.html">
 
-			</select></p>
+						</select></p>
                         <input type="submit" value="Go back!" />
-                  </form>
-				  </div>
-	</body>
+						
+            </form>
+			
+		</div>
+		
+</body>
